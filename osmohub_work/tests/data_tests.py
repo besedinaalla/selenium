432bbcda96c39bb78b112a36d8a053960771403f
besedinaalla <97469615+besedinaalla@users.noreply.github.com>
2023-08-14 23:56:45 +0300

    Update data_tests.py

--- osmohub_work/tests/data_tests.py
+++ osmohub_work/tests/data_tests.py
@@ -1,21 +1,21 @@
 class DataTest():
     implicitly_wait = 10
     wait = 30
-    login_incorrect = "Test1@remkis.ru"
-    password_incorrect = 'dSw[`r8d1d]"`#;123'
-    login_not_email = "kjuyy654!#$!"
+    login_incorrect = 
+    password_incorrect = 
+    login_not_email = 
 
-    login_correct_buyer = "Test1CD2@remkis.ru"
-    password_correct_buyer = 'dSw[`r8d1d]"`#;'
+    login_correct_buyer = 
+    password_correct_buyer = 
 
-    login_correct_seller = "Test1CD1@remkis.ru"
-    password_correct_seller = "%j5]'_s5#PWc`0?"
+    login_correct_seller = 
+    password_correct_seller = 
 
-    login_correct_dev = "test_buyer@osmocode.ru"
-    password_correct_dev = 'qwerty1234'
+    login_correct_dev = 
+    password_correct_dev = 
 
-    url_dev = "https://dev.stroycode.ru"
-    url_prod = "https://stroycode.ru"
-    url_vert = "https://158.160.52.230"
+    url_dev = 
+    url_prod = 
+    url_vert = 
 
     url = url_vert
